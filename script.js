f03de8850398d8a3260c57ec2e31b017ac79effd
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-07-28 10:49:12 +0530

    method 2 updated

--- script.js
+++ script.js
@@ -25,8 +25,34 @@
 //  Method 2
 
 
+function showKeycodes (e) {
+    const insert = document.getElementById('insert');
+    insert.innerHTML= '';
+
+    const keyCodes = {
+        'e.key' : e.key === '' ? 'Space' : e.key,
+        'e.keyCode': e.keyCode,
+        'e.code': e.code
+    };
 
 
+    for (let key in keyCodes) {
+        const div = document.createElement('div');
+        div.className = 'key';
+        const small = document.createElement('small');
 
-window.addEventListener('keydown', showkeycodes);
+        const keyText = document.createTextNode(key);
+        const valueText = document.createTextNode(keyCodes[key]);
+
+        small.appendChild(keyText);
+        div.appendChild(valueText);
+        div.appendChild(small)
+
+        insert.appendChild(div);
+    }
+
+}
+
+
+window.addEventListener('keydown', showKeycodes);
 
